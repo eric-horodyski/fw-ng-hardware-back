fb354ba266c53bf81d5fd54ab9d0652687d7ef6f
Eric Horodyski <horodyski@ionic.io>
2022-01-11 11:15:14 -0500

    Initial commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { App } from "@capacitor/app";
+import { IonRouterOutlet, Platform } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  selector: "app-tabs",
+  templateUrl: "tabs.page.html",
+  styleUrls: ["tabs.page.scss"],
 })
 export class TabsPage {
-
-  constructor() {}
-
+  constructor(
+    private routerOutlet: IonRouterOutlet,
+    private platform: Platform
+  ) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        App.exitApp();
+      }
+    });
+  }
 }
